7de3834716461eea0cefc5c3cd53a9dfa1b9c9a7
Nuria Braiza <nuriabraiza@gmail.com>
2022-01-15 11:27:23 -0300

    entrega 2

---
+++ entrega_2.js
@@ -0,0 +1,186 @@
+const fs = require("fs");
+
+class Contenedor {
+  constructor(file) {
+    this.path = "./";
+    this.file = `${file}.txt`;
+  }
+
+  async save(object) {
+    try {
+      let res = await fs.promises.readFile(`${this.path}${this.file}`, "utf-8");
+      let productos = JSON.parse(res);
+      const producto = {
+        id: productos[productos.length - 1].id + 1,
+        title: object.title,
+        price: object.price,
+        thumbnail: object.thumbnail,
+      };
+      productos.push(producto);
+      try {
+        await fs.promises.writeFile(
+          `${this.path}${this.file}`,
+          JSON.stringify(productos, null, 2)
+        );
+        return {
+          status: "Success",
+          message: "Producto creado con éxito.",
+          id: producto.id,
+        };
+      } catch (err) {
+        return {
+          status: "Error",
+          message: "Error al cargar el producto.",
+          error: err,
+        };
+      }
+    } catch (err) {
+      const producto = {
+        id: 1,
+        title: object.title,
+        price: object.price,
+        thumbnail: object.thumbnail,
+      };
+      try {
+        await fs.promises.writeFile(
+          `${this.path}${this.file}`,
+          JSON.stringify([producto], null, 2)
+        );
+        return {
+          status: "Success",
+          message: "Archivo y producto creado con éxito.",
+          id: producto.id,
+        };
+      } catch (err) {
+        return {
+          status: "Error",
+          message: "Error al crear el archivo y producto.",
+          error: err,
+        };
+      }
+    }
+  }
+
+  async getById(id) {
+    try {
+      let res = await fs.promises.readFile(`${this.path}${this.file}`, "utf-8");
+      let productos = JSON.parse(res);
+      let producto = productos.find((el) => el.id === id);
+      if (!producto) {
+        throw new Error();
+      }
+      return { status: "Success", data: producto };
+    } catch (err) {
+      return {
+        status: "Error",
+        message: "No se encontro el producto solicitado.",
+        error: err,
+        data: null,
+      };
+    }
+  }
+
+  async getAll() {
+    try {
+      let res = await fs.promises.readFile(`${this.path}${this.file}`, "utf-8");
+      let productos = JSON.parse(res);
+      return { status: "Success", data: productos };
+    } catch (err) {
+      return {
+        status: "Error",
+        message: "No se encontro el producto solicitado.",
+      };
+    }
+  }
+
+  async deleteById(id) {
+    try {
+      let res = await fs.promises.readFile(`${this.path}${this.file}`, "utf-8");
+      let productos = JSON.parse(res);
+      let siExiste = productos.find((el) => el.id === id);
+      if (!siExiste) {
+        throw new Error();
+      }
+      let productosActualizados = productos.filter((el) => el.id !== id);
+      try {
+        await fs.promises.writeFile(
+          `${this.path}${this.file}`,
+          JSON.stringify(productosActualizados, null, 2)
+        );
+        return { status: "Success", message: "Producto eliminado con éxito." };
+      } catch (err) {
+        return {
+          status: "Error",
+          message: "Hubo un problema al borrar el producto.",
+        };
+      }
+    } catch (err) {
+      return {
+        status: "Error",
+        message: "No se encontro el producto solicitado.",
+      };
+    }
+  }
+
+  async deleteAll() {
+    try {
+      await fs.promises.unlink(`${this.path}${this.file}`);
+      return {
+        status: "Success",
+        message: "Se eliminaron todos los objetos del archivo.",
+      };
+    } catch (err) {
+      return {
+        status: "Error",
+        message: "Hubo un error al intentar borrar los archivos.",
+        error: err,
+      };
+    }
+  }
+}
+
+/* ------ PRUEBA ------ */
+
+const carrito = new Contenedor("productos");
+
+// SAVE()
+/*carrito
+  .save({
+    title: "Escuadra",
+    price: 123.45,
+    thumbnail:
+      "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
+  })
+  .then(() =>
+    //Producto 2
+    carrito
+      .save({
+        title: "Calculadora",
+        price: 234.56,
+        thumbnail:
+          "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png",
+      })
+      .then(() =>
+        //Producto 3
+        carrito.save({
+          title: "Globo Terráqueo",
+          price: 345.67,
+          thumbnail:
+            "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png",
+        })
+      )
+  );*/
+
+//GET BY ID
+//carrito.getById(2).then((data) => console.log(data));
+//carrito.getById(152).then(data=>console.log(data))
+
+//DELETE BY ID
+//carrito.deleteById(1).then((data) => console.log(data));
+//carrito.deleteById(152).then(data => console.log(data))
+
+//GET ALL
+//carrito.getAll().then((data) => console.log(data));
+
+//DELETE ALL
+//carrito.deleteAll().then((data) => console.log(data));
