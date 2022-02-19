e8874e6a5fe7523ef532dbec4b560d423e70210e
Nuria Braiza <nuriabraiza@gmail.com>
2022-02-19 17:34:19 -0300

    reorganizacion + proyecto final

---
+++ proyecto_final/src/public/js/cart.js
@@ -0,0 +1,83 @@
+let cart = window.localStorage.getItem("cart");
+let cartId = -1
+if (cart !== undefined && cart !== null) {
+  cartId = JSON.parse(cart).id;
+  cart = JSON.parse(cart).products;
+
+  document.getElementById("delete-cart").addEventListener("click", () => {
+    fetch("/api/carrito/" + cartId, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.status === "success") {
+          window.localStorage.removeItem("cart");
+          window.location.reload();
+        } else {
+          Swal.fire({
+            title: "Error",
+            text: res.message,
+            icon: "error",
+            timer: 2000,
+          });
+        }
+      });
+  });
+}
+
+cartItems = [];
+
+fetch("/api/carrito/" + cartId + "/productos")
+  .then((res) => res.json())
+  .then((res) => {
+    if (res.status === "success") {
+      cartItems = res.payload.products;
+      fetch("templates/carrito.handlebars")
+        .then((response) => response.text())
+        .then((template) => {
+          const templateFn = Handlebars.compile(template);
+          const html = templateFn({ cart: cartItems });
+          const formDiv = document.getElementById("cart");
+          formDiv.innerHTML = html;
+
+          document.querySelectorAll(".delete-button").forEach((item) => {
+            item.addEventListener("click", (event) => {
+              Swal.fire({
+                title: '¿Esta seguro?',
+                text: "¿Estás seguro de eliminar este producto del carrito?",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Eliminar',
+                cancelButtonText: 'Cancelar'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  fetch(
+                    "/api/carrito/" + cartId + "/productos/" + item.dataset.id,
+                    {
+                      method: "DELETE",
+                    }
+                  ).then((_) => {
+                    Swal.fire(
+                      '¡Eliminado!',
+                      'El producto fue eliminado del carrito.',
+                      'success'
+                    ).then((_) => {
+                      window.location.reload();
+                    });
+                  });
+                }
+              })
+            });
+          });
+        });
+    } else {
+      Swal.fire({
+        title: "Error",
+        text: res.message,
+        icon: "error",
+        timer: 2000,
+      });
+    }
+  });
